0f7e282bf2d12a12a5dc552af873392ad0c82ef7
Harshal Kulkarni <42550744+harshalkulkarni07@users.noreply.github.com>
2026-07-08 21:46:42 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+         this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+getMin() {
+    if (this.stack.length === 0) return null;
+
+    let min = this.stack[0];
+
+    for (let i = 0; i < this.stack.length; i++) {
+        if (this.stack[i] < min) {
+            min = this.stack[i];
+        }
+    }
+
+    return min;
+}
+}
